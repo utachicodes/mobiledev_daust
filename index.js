43edd385d41a015f2d7869929728a214ac260a6f
utachicodes <abdoullahaljersi@gmail.com>
2026-02-06 09:28:50 +0000

    Update entry point to run LabTest1 standalone

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import { registerRootComponent } from 'expo';
 
-import App from './App';
+import LabTest1 from './src/labtest1';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(LabTest1);
---
+++ index.labtest1.js
@@ -0,0 +1,5 @@
+import { registerRootComponent } from 'expo';
+import LabTest1 from './src/labtest1';
+
+// Register LabTest1 as the root component
+registerRootComponent(LabTest1);
